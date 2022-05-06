befabcac227bf949961560cf614de8c19690f612
Peter Vietense <peter.vietense@hotmail.de>
2022-05-06 20:54:40 +0200

    refactoring

---
+++ WpfApp1/AlsoBroadcastChangeMissingAnalyzerWarning.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// I should get notified about the missing [ObservableRecipient] attribute on the class before compiling
+    /// </summary>
+    public sealed partial class AlsoBroadcastChangeMissingAnalyzerWarning : ObservableValidator
+    {
+        [ObservableProperty]
+        [AlsoBroadcastChange]
+        private object? _someProperty;
+    }
+}
--- WpfApp1/AlsoBroadcastChangeNotGenerated.cs
+++ WpfApp1/AlsoBroadcastChangeNotGenerated.cs
@@ -14,14 +14,4 @@ namespace WpfApp1
         [Display(Name = "Is static")]
         private object? _someProperty;
     }
-
-    /// <summary>
-    /// I should get notified about the missing [ObservableRecipient] attribute on the class before compiling
-    /// </summary>
-    public sealed partial class AlsoBroadcastChangeMissingAnalyzerWarning : ObservableValidator
-    {
-        [ObservableProperty]
-        [AlsoBroadcastChange]
-        private object? _someProperty;
-    }
 }
