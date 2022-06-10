8768a819886264e6c17fabdb3eb9908f768c9e16
Peter Vietense <peter.vietense@hotmail.de>
2022-06-10 11:10:00 +0200

    refactor

--- WpfLibrary1/BroadCastViewModel.cs
+++ WpfLibrary1/BroadCastViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Messaging;
-using CommunityToolkit.Mvvm.Messaging.Messages;
-using NUnit.Framework;
 
 namespace WpfLibrary1
 {
@@ -11,71 +8,5 @@ namespace WpfLibrary1
         [NotifyPropertyChangedRecipients]
         [ObservableProperty]
         private object _property;
-    }
-
-    public sealed class SourceGenObservableRecipientTests
-    {
-        [Test]
-        public void ShouldRaiseEvent()
-        {
-            var propertyChanged = false;
-
-            var viewModel = new BroadCastViewModel();
-            viewModel.PropertyChanged += ViewModel_PropertyChanged;
-            viewModel.Property = new object();
-
-            Assert.IsTrue(propertyChanged);
-
-            void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-            {
-                propertyChanged = true;
-            }
-        }
-
-        [Test]
-        public void Should_Send_Message_With_Explicit_Messenger()
-        {
-            var propertyChanged = false;
-            var messageSent = false;
-            var messenger = new StrongReferenceMessenger();
-            var viewModel = new BroadCastViewModel(messenger);
-            messenger.Register<SourceGenObservableRecipientTests, PropertyChangedMessage<object>>(this, (r, m) =>
-            {
-                messageSent = true;
-            });
-            viewModel.PropertyChanged += ViewModel_PropertyChanged;
-            viewModel.Property = new object();
-
-            Assert.IsTrue(messageSent);
-            Assert.IsTrue(propertyChanged);
-
-            void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-            {
-                propertyChanged = true;
-            }
-        }
-
-        [Test]
-        public void Should_Send_Message_Without_Explicit_Messenger()
-        {
-            var propertyChanged = false;
-            var messageSent = false;
-            var messenger = WeakReferenceMessenger.Default;
-            var viewModel = new BroadCastViewModel();
-            messenger.Register<SourceGenObservableRecipientTests, PropertyChangedMessage<object>>(this, (r, m) =>
-            {
-                messageSent = true;
-            });
-            viewModel.PropertyChanged += ViewModel_PropertyChanged;
-            viewModel.Property = new object();
-
-            Assert.IsTrue(messageSent);
-            Assert.IsTrue(propertyChanged);
-
-            void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-            {
-                propertyChanged = true;
-            }
-        }
     }
 }
