1a2791349bfd52f24e6b2d427ac627b5faf5871b
Peter Vietense <peter.vietense@hotmail.de>
2022-10-29 12:59:52 +0200

    added MaterialWindow

--- WpfApp3/MaterialWindow.cs
+++ WpfApp3/MaterialWindow.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ControlzEx;
 
 namespace WpfApp3
 {
-    public class MaterialWindow : ControlzEx.WindowChromeWindow
+    public class MaterialWindow : WindowChromeWindow
     {
     }
 }
